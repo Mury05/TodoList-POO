e3a40a002d62c4319ba3c62450bac1dafbe96a65
Mury05 <murielleodilone@gmail.com>
2024-11-22 15:34:34 +0100

    Style des nouvelles pages

--- app/Views/edit.php
+++ app/Views/edit.php
@@ -1,7 +1,7 @@
 <?php ob_start(); ?>
 <h1>Modifier la tâche</h1>
 <form action="/update?id=<?=$todoItem['id']?>" method="post">
-    <input type="text" name="task" value="<?=$todoItem['task']?>" placeholder="Modifier la tâche" required>
+    <input type="text" class="todo" name="task" value="<?=$todoItem['task']?>" placeholder="Modifier la tâche" required>
     <button type="submit">Modifier</button>
 </form>
 <?php $content = ob_get_clean(); ?>
